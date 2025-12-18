b22c75fdfcf47aa19411b2842335356504827427
Gimenez Gaitan <martin.gimenez@sofse.gob.ar>
2025-12-18 14:36:38 -0300

    app para movil

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000',
-    }
-  }
+  plugins: [
+    react(),
+    VitePWA({ registerType: 'autoUpdate' }) // Esto activa el modo instalable básico
+  ]
 })
